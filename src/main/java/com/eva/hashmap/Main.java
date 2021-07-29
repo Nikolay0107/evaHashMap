53bda84411748305e52eb3d7daa89817c8fdf7ed
Nikolay0107 <nimphonoe@gmail.com>
2021-07-29 13:03:31 +0300

    Hash map realized. Added GET value, PUT value. Implemented own hash function.

--- src/main/java/com/eva/hashmap/Main.java
+++ src/main/java/com/eva/hashmap/Main.java
@@ -3,5 +3,20 @@ package com.eva.hashmap;
 public class Main {
     public static void main(String[] args) {
 
+        HashMap<Long, Integer> map = new HashMap<>();
+
+        map.put(9223372036854775805L, 1);
+        map.put(9223372036854775806L, 2);
+        map.put(9223372036854775807L, 3);
+        map.put(-9223372036854775807L, -1);
+        map.put(-9223372036854775808L, -2);
+
+        System.out.println(map);
+
+        System.out.println(map.get(9223372036854775807L));
+
+        System.out.println(map.hasElement(922337203685477587L));
+
+
     }
 }
